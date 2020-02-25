a80ede5dfd088cd67a2a694dbe618146f6bd93b2
Tim Giles <49559033+tim-ciq@users.noreply.github.com>
2020-02-25 17:00:52 -0500

    feat: add usage of fin API

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('hello world!~');
+    console.log('hello world from preload!');
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
       if (element) element.innerText = text
@@ -10,4 +10,5 @@ window.addEventListener('DOMContentLoaded', () => {
     for (const type of ['chrome', 'node', 'electron']) {
       replaceText(`${type}-version`, process.versions[type]);
     }
+    replaceText('container-name', fin.container);
   });
--- renderer.js
+++ renderer.js
@@ -4,3 +4,19 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('hello world from renderer!');
+    fin.desktop.System.currentWindow.openDevTools();
+    let _window = fin.desktop.Application.getCurrent();
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    } 
+    
+    // for (const type of ['chrome', 'node', 'electron']) {
+    //     console.log(type);
+    //     console.log(process);
+    //   replaceText(`${type}-version`, process.versions[type]);
+    // }
+    replaceText('application-name', _window.name);
+  });
